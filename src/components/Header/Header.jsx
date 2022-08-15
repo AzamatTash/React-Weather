94f8aeb9b1da2ff91ebdfcd2d72ea30e0af10c20
Azamat <azamat300698@gmail.com>
2022-08-15 17:56:23 +0500

    #9 add change theme

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,8 +4,8 @@ import logoIcon from '../../assets/img/weather-logo.svg'
 import themeInvertIcon from '../../assets/img/theme-colors-icon.svg'
 import searchIcon from '../../assets/img/search-icon.png'
 import {connect} from 'react-redux';
-import {setCurrentCity} from '../../redux/reducers/weatherReducer';
 import {storage} from '../../storage/storage';
+import {setCurrentCity, setCurrentTheme} from "../../redux/reducers/initializationReducer";
 
 const Header = (props) => {
     const [cityName, setCityName] = React.useState();
@@ -14,11 +14,36 @@ const Header = (props) => {
         setCityName(e.currentTarget.value);
     };
 
-    const onClick = (e) => {
+    const onSend = (e) => {
         e.preventDefault();
         storage.setItem('city', cityName);
         props.setCurrentCity(cityName);
     };
+
+    const onChangeTheme = () => {
+        if (props.currentTheme === 'light') {
+            storage.setItem('theme', 'dark');
+            props.setCurrentTheme('dark');
+        } else {
+            storage.setItem('theme', 'light');
+            props.setCurrentTheme('light');
+        }
+    };
+
+    React.useEffect(() => {
+        const root = document.querySelector(':root');
+        const components = [
+            'body-background',
+            'components-background',
+            'card-background',
+            'font',
+            'components-shadow',
+            'btn-next-prev'
+        ];
+        components.forEach(component => {
+            root.style.setProperty(`--${component}-default`, `var(--${component}-${props.currentTheme})`)
+        })
+    },[props.currentTheme])
 
     return (
         <div className={classes.header}>
@@ -27,13 +52,13 @@ const Header = (props) => {
                 <h1 className={classes.title}>react weather</h1>                
             </div>
             <div className={classes.right}>
-                <button className={classes.btn}>
+                <button onClick={onChangeTheme} className={classes.btn}>
                     <img className={classes.btn_img} src={themeInvertIcon} alt='Смена темы'/>
                 </button>
                 <form className={classes.form}>
                     <input onChange={onChangeCityName} className={classes.input} placeholder='поиск...' type='text'/>
-                    <button onClick={onClick} className={classes.btn}>
-                        <img  className={classes.input_img} src={searchIcon} alt='Поиск'/>
+                    <button onClick={onSend} className={classes.btn} >
+                        <img src={searchIcon} alt='Поиск'/>
                     </button>
                 </form>
             </div>
@@ -41,4 +66,9 @@ const Header = (props) => {
     );
 };
 
-export default connect(null,{setCurrentCity})(Header);
+const mapStateToProps = (state) => {
+    return {
+        currentTheme: state.initializationReducer.currentTheme
+    }
+}
+export default connect(mapStateToProps,{setCurrentCity, setCurrentTheme})(Header);
